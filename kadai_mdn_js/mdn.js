4eaab2bd254d945527b63113faaa9fe237b12ec9
JAP-boynon <nozomin.821@gmail.com>
2025-11-11 21:51:51 +0900

    課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const date = new Date();
+const year = date.getFullYear();
+const manth = date.getMonth();
+const day = date.getDate();
+
+
+
+console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日');
+
+
