5b42c188891f91f673ae536ff23df1129cb7e3d6
JAP-boynon <nozomin.821@gmail.com>
2025-11-11 22:17:43 +0900

    再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,10 @@
 const date = new Date();
 const year = date.getFullYear();
-const manth = date.getMonth();
+const month = date.getMonth() + 1;
 const day = date.getDate();
 
 
 
-console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日');
+console.log(year + '年' + month + '月' + day+ '日');
 
 
